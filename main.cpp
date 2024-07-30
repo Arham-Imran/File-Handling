b4d951d3ad6dbe3ae4fe266092bd9ce172776315
Arham_Imran <marhamimran.mts42@gmail.com>
2024-07-30 11:27:43 +0500

    corrected naming scheme

--- main.cpp
+++ main.cpp
@@ -6,51 +6,51 @@
 #include <vector>
 using namespace std;
 
-class file_system
+class file
 {
     private:
     char file_name[50];
-    fstream file;
+    fstream file_obj;
 
     public:
     streamsize file_size;
     static const int max_size = 500;
-    file_system(int);
-    ~file_system();
+    file(int);
+    ~file();
     streamsize check_file_size();
     int write_file(string);
     int write_file(int, string);
 };
 
-file_system::file_system(int num)
+file::file(int num)
 {
     sprintf(file_name, "../test%d.txt", num);
-    file.open(file_name, ios::in | ios::out);
-    if(!file.is_open())
+    file_obj.open(file_name, ios::in | ios::out);
+    if(!file_obj.is_open())
     {
-        file.open(file_name, ios::out);
-        file.close();
-        file.open(file_name, ios::out | ios::in);
+        file_obj.open(file_name, ios::out);
+        file_obj.close();
+        file_obj.open(file_name, ios::out | ios::in);
     }
     file_size = check_file_size();
 }
 
-file_system::~file_system()
+file::~file()
 {
-    file.close();
+    file_obj.close();
 }
 
-streamsize file_system::check_file_size()
+streamsize file::check_file_size()
 {
-    file.ignore(std::numeric_limits<std::streamsize>::max());
-    streamsize length = file.gcount();
-    file.clear();
-    file.seekp(0, ios::beg);
-    file.seekg(0, ios::beg);
+    file_obj.ignore(std::numeric_limits<std::streamsize>::max());
+    streamsize length = file_obj.gcount();
+    file_obj.clear();
+    file_obj.seekp(0, ios::beg);
+    file_obj.seekg(0, ios::beg);
     return length;
 }
 
-int file_system::write_file(string new_data)
+int file::write_file(string new_data)
 {
     if(file_size >= max_size)
     {
@@ -58,13 +58,13 @@ int file_system::write_file(string new_data)
     }
     else if(new_data.size() + file_size <= max_size)
     {
-        file.close();
-        file.open(file_name, ios::out | ios::in | ios::app);
-        file << new_data;
-        file.flush();
+        file_obj.close();
+        file_obj.open(file_name, ios::out | ios::in | ios::app);
+        file_obj << new_data;
+        file_obj.flush();
         file_size += new_data.size();
-        file.close();
-        file.open(file_name, ios::out | ios::in);
+        file_obj.close();
+        file_obj.open(file_name, ios::out | ios::in);
         
         return new_data.size();
     }
@@ -72,12 +72,12 @@ int file_system::write_file(string new_data)
     {
         int buffer_size = max_size - file_size;
         new_data.resize(buffer_size);
-        file.close();
-        file.open(file_name, ios::in | ios::out | ios::app);
-        file << new_data;
-        file.flush();
-        file.close();
-        file.open(file_name, ios::out | ios::in);
+        file_obj.close();
+        file_obj.open(file_name, ios::in | ios::out | ios::app);
+        file_obj << new_data;
+        file_obj.flush();
+        file_obj.close();
+        file_obj.open(file_name, ios::out | ios::in);
         file_size += buffer_size;
 
         return buffer_size;
@@ -88,7 +88,7 @@ int file_system::write_file(string new_data)
     }
 }
 
-int file_system::write_file(int pos, string new_data)
+int file::write_file(int pos, string new_data)
 {
     if(file_size >= max_size)
     {
@@ -108,17 +108,17 @@ int file_system::write_file(int pos, string new_data)
         }
 
         stringstream file_buf;
-        file.seekg(0, ios::beg);
-        file_buf << file.rdbuf();
+        file_obj.seekg(0, ios::beg);
+        file_buf << file_obj.rdbuf();
         file_buf.seekp(pos, ios::beg);
         file_buf << new_data;
-        file.seekg(pos, ios::beg);
-        file_buf << file.rdbuf();
-        file.seekp(0, ios::beg);
-        file << file_buf.str();
-        file.flush();
-        file.seekp(0, ios::beg);
-        file.seekg(0, ios::beg);
+        file_obj.seekg(pos, ios::beg);
+        file_buf << file_obj.rdbuf();
+        file_obj.seekp(0, ios::beg);
+        file_obj << file_buf.str();
+        file_obj.flush();
+        file_obj.seekp(0, ios::beg);
+        file_obj.seekg(0, ios::beg);
         file_size += new_data.size();
         
         return new_data.size();
@@ -135,17 +135,17 @@ int file_system::write_file(int pos, string new_data)
         }
 
         stringstream file_buf;
-        file.seekg(0, ios::beg);
-        file_buf << file.rdbuf();
+        file_obj.seekg(0, ios::beg);
+        file_buf << file_obj.rdbuf();
         file_buf.seekp(pos, ios::beg);
         file_buf << new_data;
-        file.seekg(pos, ios::beg);
-        file_buf << file.rdbuf();
-        file.seekp(0, ios::beg);
-        file << file_buf.str();
-        file.flush();
-        file.seekp(0, ios::beg);
-        file.seekg(0, ios::beg);
+        file_obj.seekg(pos, ios::beg);
+        file_buf << file_obj.rdbuf();
+        file_obj.seekp(0, ios::beg);
+        file_obj << file_buf.str();
+        file_obj.flush();
+        file_obj.seekp(0, ios::beg);
+        file_obj.seekg(0, ios::beg);
         file_size += buffer_size;
 
         return buffer_size;
@@ -156,13 +156,13 @@ int file_system::write_file(int pos, string new_data)
     }
 }
 
-void create_files(vector<file_system*>& files, int num_of_files)
+void create_files(vector<file*>& files, int num_of_files)
 {
     static int num = 1;
-    file_system *new_file = NULL;
+    file *new_file = NULL;
     for(int i=0; i<num_of_files; i++)
     {
-        new_file = new file_system(num);
+        new_file = new file(num);
         files.push_back(new_file);
         num++;
     }
@@ -175,10 +175,10 @@ void parse_input(int& pos, string& line)
     line.erase(line.begin());
 }
 
-void file_control(vector<file_system*>& files, string& line, int& pos)
+void file_control(vector<file*>& files, string& line, int& pos)
 {
 
-    int files_to_create = (pos / file_system::max_size) + 1;
+    int files_to_create = (pos / file::max_size) + 1;
     if(files_to_create > files.size() && pos%500 + line.size() > 500)
     {
         create_files(files, files_to_create - files.size() + 1);        
@@ -193,7 +193,7 @@ void file_control(vector<file_system*>& files, string& line, int& pos)
         characters_written = files[i]->write_file(pos, line);
         if(characters_written <= 0)
         {
-            pos -= file_system::max_size;
+            pos -= file::max_size;
             pos = pos<0 ? 0 : pos;
             continue;
         }
@@ -208,16 +208,14 @@ void file_control(vector<file_system*>& files, string& line, int& pos)
             return;
         }
     }
-
-    // files.back()->write_file(pos%500, line);
 }
 
-// void check_prev_files(vector<file_system*>& files, string& line, )
+// void check_prev_files(vector<file*>& files, string& line, )
 // {
 //     int chars_written = 0;
 //     for(int i=files.size()-1; i>=0; i--)
 //     {
-//         if(files[i]->file_size < file_system::max_size)
+//         if(files[i]->file_size < file::max_size)
 //         {
 //             chars_written = files[i]->write_file(line);
 //             if(chars_written == line.size())
@@ -247,7 +245,7 @@ int main()
     int pos = -1;
     int chars_written = -2;
     string line;
-    vector<file_system*> files;
+    vector<file*> files;
 
     create_files(files, 4);
     
